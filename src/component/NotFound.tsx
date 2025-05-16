7e2447a6c76649781e9e417e76e886f9bd714c3e
lambda_luna <beuri97@gmail.com>
2025-05-17 02:04:13 +1200

    feat: created NotFound.tsx component

---
+++ src/component/NotFound.tsx
@@ -0,0 +1,14 @@
+import {Container, Typography} from "@mui/material";
+
+const NotFound = () => {
+
+    return (
+        <Container>
+            <Typography variant={'h1'}>
+                404 Not Found
+            </Typography>
+        </Container>
+    );
+}
+
+export default NotFound;
