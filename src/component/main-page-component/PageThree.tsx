d6aabaac2ca7c7c672e566f90201dcddbeace398
Astro-Luminoso <beuri97@gmail.com>
2025-06-05 17:03:53 +1200

    feat: initialized RecentPost card style

--- src/component/main-page-component/PageThree.tsx
+++ src/component/main-page-component/PageThree.tsx
@@ -1,5 +1,5 @@
 import {motion} from "framer-motion";
-import {Container, Grid, Typography} from "@mui/material";
+import {Box, Card, CardContent, CardMedia, Container, Divider, Grid, Typography} from "@mui/material";
 import {mainTitle} from "../../style/SxProps.ts";
 
 
@@ -7,6 +7,43 @@ const PageThree = () => {
 
 
     const MotionTypography = motion.create(Typography);
+
+
+    const recentPost  = () => {
+
+
+        return (
+            <Card
+                sx={{
+                    display: 'flex',
+                    height: '16rem',
+                    justifyContent: 'space-around',
+                    backgroundColor: 'inherit',
+                    boxShadow: 'none',
+                    borderTop: '1px solid #cecece',
+                    borderBottom: '1px solid #cecece',
+                    borderLeft: 'none',
+                    borderRight: 'none',
+                    borderRadius: 0,
+                }}
+            >
+                <Box sx={{display: 'flex', direction: 'column', textAlign: 'left'}}>
+                    <CardContent >
+                        <Typography variant={'h4'} sx={{paddingTop: '1rem', marginBottom: '1.5rem'}}>
+                            Post Title
+                        </Typography>
+                        <Typography variant={'subtitle2'} color={'text.secondary'}>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </Typography>
+                    </CardContent>
+                </Box>
+                <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+                    <CardMedia component={'img'} sx={{width: '15rem', height: '15rem', display: 'flex'}}/>
+                </Box>
+            </Card>
+        )
+
+    }
 
 
     return (
@@ -17,7 +54,7 @@ const PageThree = () => {
                       direction: 'row',
                       width: {lg: '120rem'}
                   }}>
-                <Grid size={{xs:12, lg:6}}>
+                <Grid size={{xs:12, lg:7}}>
                     <MotionTypography
                         sx={{...mainTitle, textAlign: 'left'}}
                         initial={{ opacity: 0, x: -20 }}
@@ -27,8 +64,21 @@ const PageThree = () => {
                     >
                         Recent Posts
                     </MotionTypography>
+                    {/*TODO: Remove this hardcoded post cards when all hooks are ready*/}
+                    {recentPost()}
+                    {recentPost()}
                 </Grid>
-                <Grid size={{xs:12, lg:6}}>
+                <Grid
+                    size={{lg:1}}
+                    sx={{
+                        display: {xs: 'none', lg:'flex'},
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                        }}
+                >
+                    <Divider orientation={'vertical'} flexItem/>
+                </Grid>
+                <Grid size={{xs:12, lg:4}}>
                     <MotionTypography
                         sx={{...mainTitle, textAlign: 'left'}}
                         initial={{ opacity: 0, x: 20 }}
@@ -38,11 +88,9 @@ const PageThree = () => {
                     >
                         Albums
                     </MotionTypography>
+                    <Divider/>
                 </Grid>
-
             </Grid>
-
-
         </Container>
     )
 
