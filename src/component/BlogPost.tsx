bcf495a939366c0137e3c994b5220d1c69210c5e
Astro-Luminoso <beuri97@gmail.com>
2025-07-26 00:40:33 +0900

    feat: created empty table for Desktop/Laptop

--- src/component/BlogPost.tsx
+++ src/component/BlogPost.tsx
@@ -1,6 +1,6 @@
-import {Container, Typography} from "@mui/material";
-import {motion} from "framer-motion";
-import {mainTitle} from "../style/SxProps.ts";
+import {Container, Paper, Table, TableCell, TableContainer, TableHead, TableRow, Typography} from "@mui/material";
+import { motion } from "framer-motion";
+import { mainTitle } from "../style/SxProps.ts";
 
 
 const BlogPost = () => {
@@ -8,16 +8,32 @@ const BlogPost = () => {
     const MotionTypography = motion.create(Typography);
 
     return (
-        <Container maxWidth={false}>
+        <Container maxWidth={false} >
             <MotionTypography
-                sx={{...mainTitle, textAlign: 'left'}}
-                initial={{opacity: 0, y: 30}}
+                sx={{...mainTitle,  textAlign: 'left'}}
+                initial={{opacity: 0, y: -30}}
                 animate={{opacity: 1, y: 0}}
                 exit={{opacity: 0, y: -30, transition: {duration: 0.5}}}
                 transition={{duration: 2}}>
-                    All Posts
+                All Posts
             </MotionTypography>
+            <TableContainer component={Paper}
+                            elevation={0}
+                            sx={{
+                                width: '100%',
+                                border: '1px solid #CECECE',
 
+                            }}>
+                <Table aria-label={"blog posts table"}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell sx={{width:'60%'}}>Title</TableCell>
+                            <TableCell sx={{width:'20%'}}>Category</TableCell>
+                            <TableCell sx={{width:'20%'}}>Date</TableCell>
+                        </TableRow>
+                    </TableHead>
+                </Table>
+            </TableContainer>
         </Container>
     )
 }
