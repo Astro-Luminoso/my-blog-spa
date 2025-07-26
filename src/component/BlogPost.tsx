791d9cfbfbd24450e1e5768dd153d16c19b1ffcc
Astro-Luminoso <beuri97@gmail.com>
2025-07-26 23:29:10 +0900

    feat: added TablePagination and filter icon in Filter button

--- src/component/BlogPost.tsx
+++ src/component/BlogPost.tsx
@@ -4,15 +4,16 @@ import {
     Paper,
     Table,
     TableCell,
-    TableContainer,
-    TableHead,
+    TableContainer, TableFooter,
+    TableHead, TablePagination,
     TableRow,
     Typography
 } from "@mui/material";
+import FilterListIcon from "@mui/icons-material/FilterList";
 
 import { motion } from "framer-motion";
 import {buttonReaction, mainTitle} from "../style/SxProps.ts";
-import type {FC} from "react";
+import {type ChangeEvent, type FC, useState} from "react";
 
 type TableRowBuilderProps = {
     row1: string;
@@ -31,10 +32,17 @@ const TableRowBuilder: FC<TableRowBuilderProps> = ({row1, row2, row3}:TableRowBu
     )
 }
 
+const MotionTypography = motion.create(Typography);
+const MotionButton = motion.create(Button);
+
 const BlogPost = () => {
 
-    const MotionTypography = motion.create(Typography);
-    const MotionButton = motion.create(Button);
+    const [rowsPerPage, setRowsPerPage] = useState(8);
+    const handleChangeRowsPerPage = (
+        event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    ) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+    };
 
 
 
@@ -65,13 +73,14 @@ const BlogPost = () => {
                     }}
                 >
                     Filter
+                    <FilterListIcon sx={{marginLeft: '0.5rem'}}/>
                 </MotionButton>
             </Box>
             <Box sx={{ display: 'flex', justifyContent: 'center' }}>
                 <TableContainer component={Paper}
                                 elevation={0}
                                 sx={{
-                                    height: `${56 * ( 8+ 1)}px`,
+                                    height: `${56 * ( 9+ 1)}px`,
                                     border: '2px solid #CECECE',
                                     justifyContent: 'center',
                                     borderLeft: 'none',
@@ -84,6 +93,18 @@ const BlogPost = () => {
                         <TableHead>
                             <TableRowBuilder row1={"Title"} row2={"Category"} row3={"Date Issued"}/>
                         </TableHead>
+                        <TableFooter>
+                            <TablePagination
+                                rowsPerPageOptions={[8, 16, 25]}
+                                count={100} // This should be the total number of posts
+                                colSpan={3}
+                                rowsPerPage={rowsPerPage}
+                                page={0}
+                                onPageChange={undefined}
+                                onRowsPerPageChange={handleChangeRowsPerPage}
+
+                            />
+                        </TableFooter>
                     </Table>
                 </TableContainer>
             </Box>
